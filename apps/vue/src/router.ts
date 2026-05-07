63755cae3b69c80af18f0976153b1d3cb0a42f54
Jamie Reynolds <jamusreynolds@gmail.com>
2026-05-07 14:02:40 +0100

    Chore - Add project linting and type check commit hook (#6)
    * typecheck
    
    * Update package.json
    
    * linting

--- apps/vue/src/router.ts
+++ apps/vue/src/router.ts
@@ -1,9 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from './views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "./views/HomeView.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', component: HomeView },
-  ],
-})
+  routes: [{ path: "/", component: HomeView }],
+});
