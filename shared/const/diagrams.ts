7adee3cd03737285681e3b4199385a9309558abc
Jamie Reynolds <jamusreynolds@gmail.com>
2026-05-14 10:32:32 +0100

    updated Starship diagram

--- shared/const/diagrams.ts
+++ shared/const/diagrams.ts
@@ -2,7 +2,7 @@ import raw14 from "@shared/assets/images/diagrams/falcon-9v1.1.svg?raw";
 import raw26 from "@shared/assets/images/diagrams/electron.svg?raw";
 import raw128 from "@shared/assets/images/diagrams/128.svg?raw";
 import raw143 from "@shared/assets/images/diagrams/143.svg?raw";
-import raw522 from "@shared/assets/images/diagrams/starship-v3.svg?raw";
+import raw527 from "@shared/assets/images/diagrams/starship-v2.svg?raw";
 import {
   parseSvgPaths,
   parseSvgStages,
@@ -38,5 +38,5 @@ export const diagrams: { [key: number]: DiagramEntry } = {
   26: makeDiagramEntry(raw26),
   128: makeDiagramEntry(raw128),
   143: makeDiagramEntry(raw143),
-  522: makeDiagramEntry(raw522),
+  527: makeDiagramEntry(raw527),
 };
