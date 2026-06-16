cfd2a889e34c71cc85274d655381d071558c5dfd
Jamie Reynolds <jamusreynolds@gmail.com>
2026-06-16 17:56:53 +0100

    add project theme and css

--- apps/angular/src/app/app.ts
+++ apps/angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AppShellComponent } from "./components/app-shell.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppShellComponent],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
 })
