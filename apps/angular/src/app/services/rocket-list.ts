bf4611eb7143881a5e54cc6a1cd886682871c903
Jamie Reynolds <jamusreynolds@gmail.com>
2026-06-23 17:30:09 +0100

    Update rocket-list.ts

--- apps/angular/src/app/services/rocket-list.ts
+++ apps/angular/src/app/services/rocket-list.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable, signal } from "@angular/core";
-import { Apollo, gql } from "apollo-angular";
+import { Apollo } from "apollo-angular";
 import { finalize, map } from "rxjs";
 
 import {
